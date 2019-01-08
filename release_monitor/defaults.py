d9b41c5b566a126796d92333e953cfc15c7b17d1
Michal Srb <michal@redhat.com>
2019-01-08 20:36:56 +0100

    Make sure DEBUG is actually a boolean

--- release_monitor/defaults.py
+++ release_monitor/defaults.py
@@ -3,9 +3,8 @@ import os
 
 NPM_URL = os.environ.get('NPM_URL', 'https://registry.npmjs.org/')
 PYPI_URL = os.environ.get('PYPI_URL', 'https://pypi.org/')
-ENABLE_SCHEDULING = os.environ.get('ENABLE_SCHEDULING', '1')\
-                    in ('1', 'True', 'true')
+ENABLE_SCHEDULING = os.environ.get('ENABLE_SCHEDULING', 'true').lower() in ('true', 'yes', '1')
 PROBE_FILE_LOCATION = "/tmp/release_monitoring/liveness.txt"
 # Sleep interval in minutes.
 SLEEP_INTERVAL = os.environ.get('SLEEP_INTERVAL', 20)
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', 'false').lower() in ('true', 'yes', '1')
